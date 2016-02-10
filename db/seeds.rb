add1f4e0c0ae710ea55a8e89d32a2c5e61d985b8
jdmor <jona.morrissey@gmail.com>
2016-02-09 19:16:35 -0800

    Add missing comma to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ contacts = [
   ['William', 'Lowell', '219-793-1840', 'williamjlowell@teleworm.us'],
   ['Brandon', 'Hayes', '847-386-2853', 'brandonbhayes@armyspy.com'],
   ['Amanda', 'Castro', '847-558-3885', 'amandajcastro@rhyta.com'],
-  ['Micheal', 'Kelley' '407-426-1894', 'michealjkelley@gmail.com'],
+  ['Micheal', 'Kelley', '407-426-1894', 'michealjkelley@gmail.com'],
   ['Michale', 'Buckner', '704-395-1860', 'michalelbuckner@hotmail.com']
 ]
 
