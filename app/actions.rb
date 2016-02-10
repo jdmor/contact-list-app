31bfc97228f012b335f1c4c13697d322a6f50d5e
jdmor <jona.morrissey@gmail.com>
2016-02-09 17:24:31 -0800

    Built create contact feature

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,19 @@
-# Homepage (Root path)
 get '/' do
   erb :index
+end
+
+post '/api/contacts/new' do
+  results = {result: false}
+
+  contact = Contact.new(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    phone: params[:phone]
+  )
+
+  if contact.save!
+    results[:result] = true
+  end
+  results.to_json
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,31 @@
-$(document).ready(function() {
+'use strict';
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(function() {
+
+
+$('#showNewContactForm').on('click', function() {
+  $('#createContact').show();
+});
+
+$('#createContact').on('submit', function() {
+
+  var firstName = $('#contactFirstName').val();
+  var lastName = $('#contactLastName').val();
+  var email = $('#contactEmail').val();
+  var phone = $('#contactPhone').val();
+
+  $.post('/api/contacts/new',
+    {first_name: firstName, last_name: lastName, email: email, phone: phone},
+    function(response) {
+      if (response.result) {
+        alert('Yay it worked');
+      } else {
+        alert('Didn\'t work :(');
+      }
+    }, 'json');
+
+  return false;
+
+});
+
 });
